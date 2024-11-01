d6ec915ce67762521fb8936e6331783bb5497528
dobby-zent <dobby.lee@zent.kr>
2024-11-01 14:16:31 +0900

    add to homebrew-tap-wip

---
+++ wip.rb
@@ -0,0 +1,10 @@
+class Gohonglab < Formula
+    desc "Wip"
+    homepage "https://github.com/zkfmapf123/Wip"
+    url "https://github.com/zkfmapf123/Wip/releases/download/v1.0.5/wip"
+    sha256 "e99cab27de78f53448f4007c265a503257e2fff36221d4ceb58b783414a23a11"
+  
+    def install
+      bin.install "wip"
+    end
+  end
