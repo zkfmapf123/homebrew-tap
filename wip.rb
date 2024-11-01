4c18b6db7b31ba4e8c0f24d63f3b0930c35159ce
Lee Dong Gyu <47292546+zkfmapf123@users.noreply.github.com>
2024-11-01 14:33:58 +0900

    Update wip.rb

--- wip.rb
+++ wip.rb
@@ -2,7 +2,7 @@ class Wip < Formula
     desc "Wip"
     homepage "https://github.com/zkfmapf123/Wip"
     url "https://github.com/zkfmapf123/Wip/releases/tag/v1.0.9"
-    sha256 "fa22933bb766f37c9ac1ae30d0ef6fdf0310094c82687188f87f8de46a493b54"
+    sha256 "c8ded9a492e87e10700437c5a267df90d6a28f1b31c6c7838a233087d69ab615"
   
     def install
       bin.install "wip"
