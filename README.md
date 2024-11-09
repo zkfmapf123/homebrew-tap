947428083295015ad4232ed21631b14d88576f8c
zkfmapf123 <zkfmapf123@naver.com>
2024-11-09 11:14:59 +0900

    add to dtf

---
+++ README.md
@@ -0,0 +1,9 @@
+# zkfmapf123 homebrew tap
+
+## Wip
+
+<a href="https://github.com/zkfmapf123/Wip"> Wip Git Repositroy </a>
+
+## dtf
+
+<a href="https://github.com/zkfmapf123/dtf"> dft Git Repository </a>
---
+++ dft.rb
@@ -0,0 +1,10 @@
+class dft < Formula
+    desc "dft"
+    homepage "https://github.com/zkfmapf123/dtf"
+    url "https://github.com/zkfmapf123/dft/releases/download/v1.0.0/dft"
+    sha256 "73cc8dd31255727db693cb957c1a7e47f777aa68887865f6649f7533cbe1a3ba"
+  
+    def install
+      bin.install "dft"
+    end
+  end
