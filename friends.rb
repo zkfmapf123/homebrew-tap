133f382e5c2455c39ce69b0bf6533bd8e16ea4cd
zkfmapf123 <zkfmapf123@naver.com>
2024-12-23 00:53:00 +0900

    wip-14086e7b

---
+++ friends.rb
@@ -0,0 +1,10 @@
+class Friends < Formula
+    desc "Wip"
+    homepage "https://github.com/zkfmapf123/Wip"
+    url "https://github.com/zkfmapf123/Wip/releases/download/v1.0.11/wip"
+    sha256 "daf5f11a375cc1a0ec8cad8f73b6e316c63a68e70e12a3ee34f1a89ead55f609"
+  
+    def install
+      bin.install "wip"
+    end
+  end
