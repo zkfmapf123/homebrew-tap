b9e458f4e197a49d7ef9aa82ed41d6789c38d2fd
leedonggyu <zkfmapf123@naver.com>
2025-04-06 22:41:11 +0900

    wip-62439e1a

--- ecsctl.rb
+++ ecsctl.rb
@@ -1,7 +1,7 @@
 class Ecsctl < Formula
-    desc "ecs orchestration tool"
+    desc "Ecsctl"
     homepage "https://github.com/zkfmapf123/ecsctl"
-    url "https://github.com/zkfmapf123/ecsctl/releases/tag/v1.0.3/ecsctl-darwin-arm64"
+    url "https://github.com/zkfmapf123/ecsctl/releases/download/v1.0.3/ecsctl-darwin-arm64"
     sha256 "473c676ecf9aa6620643a3421a6050445ffa1375c13d58de1ac564bb87e683f4"
   
     def install
