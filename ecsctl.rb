46bd0af25c44e6ab28ae40a76759061db24870dd
leedonggyu <zkfmapf123@naver.com>
2025-04-06 22:25:13 +0900

    wip-53cf8484

--- ecsctl.rb
+++ ecsctl.rb
@@ -1,8 +1,8 @@
 class Ecsctl < Formula
     desc "ecs orchestration tool"
     homepage "https://github.com/zkfmapf123/ecsctl"
-    url "https://github.com/zkfmapf123/ecsctl/releases/tag/v1.0.2/ecsctl"
-    sha256 "3a0aa1f42efbd853f1c51b78266669e6cc2e14b6b72fa77bf63b8b693b1bc52c"
+    url "https://github.com/zkfmapf123/ecsctl/releases/tag/v1.0.3/ecsctl"
+    sha256 "abe95a477ab7dbc26e6129d851162badad6e008f306ce4bfd3f4fde73cec3316"
   
     def install
       bin.install "ecsctl"
